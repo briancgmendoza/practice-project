7f2c0d7c54f6111e7390cfe11d07b8808170cb94
Mendoza Brian Christopher (ERNI) <brian.mendoza@vendorpartners.com.ph>
2022-12-02 15:32:49 +0800

    Clean/Ov-001: Cleaning code and fixing some ESLint Error

--- src/modules/login/login.tsx
+++ src/modules/login/login.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from 'react'
 // import { Link } from 'react-router-dom';
-import { Dashboard } from '../dashboard/dashboard';
-import  '../../styles/_login.scss';
-import ProgressiveImage from 'react-progressive-graceful-image';
-import { useDispatch, useSelector } from 'react-redux';
-import { system } from '../../utils/constants';
-import { Tooltip, IconButton } from '@mui/material';
-import InfoIcon from '@material-ui/icons/Info';
+import '../../styles/_login.scss'
+import ProgressiveImage from 'react-progressive-graceful-image'
+import { useDispatch } from 'react-redux'
+import { IconButton, Tooltip } from '@mui/material'
+import InfoIcon from '@material-ui/icons/Info'
 import {
   GoogleLogin,
   GoogleLoginResponse,
   GoogleLoginResponseOffline,
-} from 'react-google-login';
-import { actionCreators } from '../reducer';
+} from 'react-google-login'
+import { system } from '../../utils/constants'
+import { actionCreators } from '../reducer'
 
 export function Login() {
-  const dispatch = useDispatch();
-  
+  const dispatch = useDispatch()
+
   const handleSuccess = (
     response: GoogleLoginResponse | GoogleLoginResponseOffline
   ) => {
-    const { googleId, profileObj } = response as GoogleLoginResponse;
-    const { imageUrl } = profileObj;
-    const { tokenId: googleIdToken } = response as GoogleLoginResponse;
-    dispatch(actionCreators.googleLogin({ googleId, googleIdToken, imageUrl }));
+    const { googleId, profileObj } = response as GoogleLoginResponse
+    const { imageUrl } = profileObj
+    const { tokenId: googleIdToken } = response as GoogleLoginResponse
+    dispatch(actionCreators.googleLogin({ googleId, googleIdToken, imageUrl }))
   }
 
   const handleFailure = () => {
     dispatch(actionCreators.googleLoginFailed())
   }
 
-  const testId = "169589393731-fcq6osljk6rq7hta5t5mcumtkf963r60.apps.googleusercontent.com";
+  const testId =
+    '169589393731-fcq6osljk6rq7hta5t5mcumtkf963r60.apps.googleusercontent.com'
 
   return (
     <div className="auth-login" data-testid="login-page-title">
@@ -49,14 +49,29 @@ export function Login() {
                   <div className="container">
                     <div className="banner">
                       <div className="login-format">
-                        <img className="entrego-logo" src="https://rec-data.kalibrr.com/logos/LDC3AY6PVYPWEJD4SMTGGC63GX96WNTQMGSHWJBX-5bad80fb.png"></img>
-                        <h1 style={{fontSize: 35, transform: "rotate(-25deg)", position: 'fixed', float: "inline-start", marginTop: '65px'}}>Shop</h1>
+                        <img
+                          className="entrego-logo"
+                          src="https://rec-data.kalibrr.com/logos/LDC3AY6PVYPWEJD4SMTGGC63GX96WNTQMGSHWJBX-5bad80fb.png"
+                          alt=""
+                        />
+                        <h1
+                          style={{
+                            fontSize: 35,
+                            transform: 'rotate(-25deg)',
+                            position: 'fixed',
+                            float: 'inline-start',
+                            marginTop: '65px',
+                          }}
+                        >
+                          Shop
+                        </h1>
                         <div className="email-disclaimer">
-                          <span>Please login using your </span><strong>registered work e-mail</strong>
+                          <span>Please login using your </span>
+                          <strong>registered work e-mail</strong>
                           <span>
                             <Tooltip title={system.LOGIN_EMAIL_DISCLAIMER}>
                               <IconButton aria-label="info-email-disclaimer">
-                                 <InfoIcon color="primary" fontSize="small" />
+                                <InfoIcon color="primary" fontSize="small" />
                               </IconButton>
                             </Tooltip>
                           </span>
@@ -70,15 +85,13 @@ export function Login() {
                             onFailure={handleFailure}
                           />
                         </div>
-                        <div>
-
-                        </div>
+                        <div />
                         <div className="text-underline">
                           {/* <Link
                             to="/terms"
                             className="text-underline"
                           > */}
-                            <pre>Terms of use. Privacy policy.</pre>
+                          <pre>Terms of use. Privacy policy.</pre>
                           {/* </Link> */}
                         </div>
                       </div>
@@ -86,7 +99,6 @@ export function Login() {
                   </div>
                 </div>
               </div>
-
             </div>
           )}
         </ProgressiveImage>
